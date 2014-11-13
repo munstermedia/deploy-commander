fd5f2733445d829f307bbdc5f74a9a1f8f31f053
Ference <ference@munstermedia.nl>
2014-11-13 20:50:09 +0100

    Added skeleton files

---
+++ skeleton/example1/.templates/project/config.php
@@ -0,0 +1,7 @@
+<?php 
+define('DB_DATABASE', '{{ DB_DATABASE }}');
+define('DB_USER', '{{ DB_USER }}');
+define('DB_PASSWORD', '{{ DB_PASSWORD }}');
+
+define('ENVIRONMENT', '{{ ENVIRONMENT }}');
+?>
