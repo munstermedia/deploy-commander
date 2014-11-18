badae34b42fd62d806be0d94cc236f11e6140920
Ference <ference@munstermedia.nl>
2014-11-18 21:44:40 +0100

    Removed unused skeleton

--- skeleton/README.md
+++
@@ -1,3 +0,0 @@
-#Skeleton examples
-
-Here you can find different examples how to solve certain challenges
--- skeleton/example1/.templates/project/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define('DB_DATABASE', '{{ DB_DATABASE }}');
-define('DB_USER', '{{ DB_USER }}');
-define('DB_PASSWORD', '{{ DB_PASSWORD }}');
-
-define('ENVIRONMENT', '{{ ENVIRONMENT }}');
-?>
